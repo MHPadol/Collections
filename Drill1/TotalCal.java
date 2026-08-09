69e7f6f091c97c30378c8e9b268fb8a7fc57a9bc
MHPadol <mrryhzll@gmail.com>
2026-08-09 15:42:41 +0800

    Add TotalCal class to compute and print total

---
+++ Drill1/TotalCal.java
@@ -0,0 +1,14 @@
+public class TotalCal {
+public static void main (String []args ) {
+
+// 2 variables
+int x = 80;
+int y = 21;
+
+//compute total
+int total = x+y;
+
+// print result
+System.out.println("Total: " + total);
+        }
+}
