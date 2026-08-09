4aa752fcf93e192e50bf81231913766e55e018fb
pastelpixel <mrryhzll@gmail.com>
2026-08-09 19:47:42 +0800

    Implement binary search in BigOEx.java

---
+++ aug03_midterms_DSAA_BigO/BigOEx.java
@@ -0,0 +1,41 @@
+public class BigOEx {
+
+    public static int search(int[] numbers, int target) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left <= right) {
+
+            int middle = (left + right) / 2;
+
+            if (numbers[middle] == target) {
+                return middle;
+            } 
+            else if (numbers[middle] < target) {
+                left = middle + 1;
+            } 
+            else {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50, 60, 70};
+        int target = 50;
+
+        int result = search(numbers, target);
+
+        if (result != -1) {
+            System.out.println(
+                "🔍 " + target + " found at index " + result
+            );
+        } else {
+            System.out.println("❌ Number not found.");
+        }
+    }
+}
